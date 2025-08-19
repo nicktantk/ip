b12cce1532554de8352e9e581e8ab7467f3a2348
Nicholas Tan <nicktantk@gmail.com>
2025-08-20 01:52:13 +0800

    level 0

---
+++ src/main/java/Chlo.java
@@ -0,0 +1,10 @@
+public class Chlo {
+    public static void main(String[] args) {
+        String line = "-".repeat(50);
+        System.out.println(line);
+        System.out.println("Hello! I'm Chlo\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
