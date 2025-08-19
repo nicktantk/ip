49502212dd18f38b42f180bc84e66b167064623b
Nicholas Tan <nicktantk@gmail.com>
2025-08-20 02:09:54 +0800

    level 2

--- src/main/java/Chlo.java
+++ src/main/java/Chlo.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Chlo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks =  new ArrayList<>();
         String line = "-".repeat(50);
         System.out.println(line + "\nHello! I'm Chlo\nWhat can I do for you?\n" + line);
 
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equals("bye")) {
                 System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
+                }
+                System.out.println(line);
+            } else {
+                tasks.add(input);
+                System.out.println(line + "\nadded: " + input + "\n" + line);
             }
-            System.out.println(line + "\n" + input + "\n" + line);
         }
 
         scanner.close();
