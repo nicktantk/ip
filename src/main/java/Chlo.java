0ab271c76644b7cffc63ecffd349586d78c72fcf
Nicholas Tan <nicktantk@gmail.com>
2025-08-20 01:58:53 +0800

    level 1

--- src/main/java/Chlo.java
+++ src/main/java/Chlo.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Chlo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "-".repeat(50);
-        System.out.println(line);
-        System.out.println("Hello! I'm Chlo\nWhat can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(line + "\nHello! I'm Chlo\nWhat can I do for you?\n" + line);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            System.out.println(line + "\n" + input + "\n" + line);
+        }
+
+        scanner.close();
     }
 }
