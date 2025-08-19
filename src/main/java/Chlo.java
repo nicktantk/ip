5e68484f6fe61a7dfcd872cbc87cce6d5719bf3d
Nicholas Tan <nicktantk@gmail.com>
2025-08-20 02:29:11 +0800

    level 3

--- src/main/java/Chlo.java
+++ src/main/java/Chlo.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class Chlo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> tasks =  new ArrayList<>();
+        Task[] tasks =  new Task[100];
+        int count = 0;
         String line = "-".repeat(50);
         System.out.println(line + "\nHello! I'm Chlo\nWhat can I do for you?\n" + line);
 
@@ -17,12 +18,21 @@ public class Chlo {
                 break;
             } else if (input.equals("list")) {
                 System.out.println(line);
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + ". " + tasks.get(i));
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i+1) + ". " + tasks[i].toString());
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark")) {
+                int i = Integer.parseInt(input.split(" ")[1]);
+                tasks[i-1].markDone();
+                System.out.println(line + "\nNice! I've marked this task as done:\n" + tasks[i-1].toString() + "\n" + line);
+            } else if (input.startsWith("unmark")) {
+                int i = Integer.parseInt(input.split(" ")[1]);
+                tasks[i-1].markUndone();
+                System.out.println(line + "\nOK, I've marked this task as not done yet:\n" + tasks[i-1].toString() + "\n" + line);
+
             } else {
-                tasks.add(input);
+                tasks[count++] =  new Task(input);
                 System.out.println(line + "\nadded: " + input + "\n" + line);
             }
         }
