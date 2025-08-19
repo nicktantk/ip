9854aa4cfb5e14d60514ef54c9602ff6a2645f88
Nicholas Tan <nicktantk@gmail.com>
2025-08-20 02:50:02 +0800

    level 4

--- src/main/java/Chlo.java
+++ src/main/java/Chlo.java
@@ -18,10 +18,23 @@ public class Chlo {
                 break;
             } else if (input.equals("list")) {
                 System.out.println(line);
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < count; i++) {
                     System.out.println((i+1) + ". " + tasks[i].toString());
                 }
                 System.out.println(line);
+            } else if (input.startsWith("todo")) {
+                tasks[count++] = new Todo(input.substring(5));
+                System.out.println(line + "\nGot it. I've added this task:\n" + tasks[count-1].toString() + "\nNow you have " + count + " tasks in the list.\n" + line);
+            } else if (input.startsWith("deadline")) {
+                int by = input.indexOf("/by");
+                tasks[count++] = new Deadline(input.substring(9, by-1), input.substring(by+4));
+                System.out.println(line + "\nGot it. I've added this task:\n" + tasks[count-1].toString() + "\nNow you have " + count + " tasks in the list.\n" + line);
+            } else if (input.startsWith("event")) {
+                int from = input.indexOf("/from");
+                int to = input.indexOf("/to");
+                tasks[count++] = new Event(input.substring(6, from-1), input.substring(from+6, to-1), input.substring(to+4));
+                System.out.println(line + "\nGot it. I've added this task:\n" + tasks[count-1].toString() + "\nNow you have " + count + " tasks in the list.\n" + line);
             } else if (input.startsWith("mark")) {
                 int i = Integer.parseInt(input.split(" ")[1]);
                 tasks[i-1].markDone();
