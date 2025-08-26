a0c8ab857ea2e7c1ac7ea5e23ab9889376a85c6d
Nicholas Tan <nicktantk@gmail.com>
2025-08-26 16:25:32 +0800

    Remove deleted files

--- src/main/java/chlo/exception/ChloException.java
+++
@@ -1,9 +0,0 @@
-package chlo.exception;
-
-import java.io.IOException;
-
-public class ChloException extends IOException {
-    public ChloException(String message){
-        super(message);
-    }
-}
