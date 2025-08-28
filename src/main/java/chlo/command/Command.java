dd6e358cd9830d79b9473ebca329c3da375dffcf
Nicholas Tan <nicktantk@gmail.com>
2025-08-28 09:18:50 +0800

    Add more coding standards to command files

--- src/main/java/chlo/command/Command.java
+++ src/main/java/chlo/command/Command.java
@@ -1,13 +1,20 @@
 package chlo.command;
 
-import chlo.storage.*;
-import chlo.task.*;
-import chlo.ui.*;
+import chlo.storage.Storage;
+import chlo.task.TaskList;
+import chlo.ui.Ui;
+
 /**
  * Represents an abstract command class that gives template of all task classes.
  * Task classes: Event, Todo, Deadline
  */
 public abstract class Command {
+    /**
+     * Carries out the command with tasks, ui, storage taking changes
+     * @param tasks
+     * @param ui
+     * @param storage
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
 
     public boolean isExit() { return false; }
